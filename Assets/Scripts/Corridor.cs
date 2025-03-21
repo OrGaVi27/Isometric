e6f8ebe0c28cb9de9dac6c9bfc25f03959cf3e0d
OrGaVi27 <calgali27@gmail.com>
2025-03-21 19:22:55 +0100

    Bridge puzle beta done
    i made a beta and a build, next thing is attack

--- Assets/Scripts/Corridor.cs
+++ Assets/Scripts/Corridor.cs
@@ -1,8 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using Unity.VisualScripting;
-using UnityEditor.Animations;
 using UnityEngine;
 
 public class Corridor : MonoBehaviour
@@ -13,23 +8,12 @@ public class Corridor : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            StartCoroutine(LoadScene());
-        }
-    }
-
-
-    IEnumerator LoadScene()
-    {
-        //Evento para llamar al fade Para cambiar de habitación
-        GameEvents.current.RoomFadeTrigger();
-
-        yield return new WaitForSeconds(0.25f);
+            //Evento para llamar al fade Para cambiar de habitación
+            GameEvents.current.RoomFadeTrigger();
         
-        //Activar la habitación adyacente y desactivar la actual
-        adjacentRoom.GetComponent<MeshRenderer>().enabled = true;
-        foreach (Transform child in adjacentRoom.transform) child.gameObject.SetActive(true);
-
-        originRoom.GetComponent<MeshRenderer>().enabled = false;
-        foreach (Transform child in originRoom.transform) child.gameObject.SetActive(false);
+            //Activar la habitación adyacente y desactivar la actual
+            adjacentRoom.SetActive(true);
+            originRoom.SetActive(false);
+        }
     }
 }
